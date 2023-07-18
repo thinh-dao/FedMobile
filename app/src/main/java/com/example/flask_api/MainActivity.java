ffb83a0b722dbd657da052383ec558f785ef4d16
Thinh Dao <ducthinh19102003@gmail.com>
2023-07-18 23:19:13 +0700

    Update project

--- app/src/main/java/com/example/flask_api/MainActivity.java
+++ app/src/main/java/com/example/flask_api/MainActivity.java
@@ -5,8 +5,8 @@ import androidx.annotation.NonNull;
 
 import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
 import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -20,6 +20,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
@@ -41,18 +51,21 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-
     private EditText address;
     private EditText port;
     private EditText client;
+    private EditText nEpochs;
     private TextView status;
     private Button connectBtn;
     private Button loadDataBtn;
     private Button startTrainingBtn;
-    private String url = "http://";
+    private String url;
     private final String POST = "POST";
     private final String GET = "GET";
     private TransferLearning tl;
+    private int current_round = 1;
+    private String project_name;
+    FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,82 +80,128 @@ public class MainActivity extends AppCompatActivity {
         address = (EditText) findViewById(R.id.addressID);
         port = (EditText) findViewById(R.id.portID);
         client = (EditText) findViewById(R.id.client_id);
+        nEpochs = (EditText) findViewById(R.id.epochs_ID);
 
         connectBtn = (Button) findViewById(R.id.connectID);
-        loadDataBtn = (Button) findViewById(R.id.dataLoaderID);
+        loadDataBtn = (Button) findViewById(R.id.loadDataID) ;
         startTrainingBtn = (Button) findViewById(R.id.trainingID);
-
 
         tl = new TransferLearning(
                 getApplicationContext(),
                 "model",
                 Arrays.asList(CIFAR10BatchFileParser.getClasses()),
                 10);
+        storage = FirebaseStorage.getInstance();
     }
-
     public void connect(View view) {
+        hideKeyboard(this);
         if (TextUtils.isEmpty(address.getText().toString())) {
             Toast.makeText(this, "Please enter the server IP", Toast.LENGTH_SHORT).show();
         }
-        else if (TextUtils.isEmpty(port.getText().toString())) {
-            Toast.makeText(this, "Please enter the Port", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(port.getText().toString())) {
+            Toast.makeText(this, "Please enter the server port", Toast.LENGTH_SHORT).show();
         }
-        else {
-            url += address.getText().toString() + ":" + port.getText().toString();
-            setResultText("Connecting to server...");
-            sendRequest(GET, "check", null, null);
-        }
-    }
-    public void loadData(View view) {
-        if (TextUtils.isEmpty(client.getText().toString())) {
-            Toast.makeText(this, "Please enter a client partition ID between 1 and 10 (inclusive)", Toast.LENGTH_LONG).show();
+        else if (TextUtils.isEmpty(client.getText().toString())) {
+            Toast.makeText(this, "Please enter a client partition ID", Toast.LENGTH_SHORT).show();
         }
         else if (Integer.parseInt(client.getText().toString()) > 10 ||  Integer.parseInt(client.getText().toString()) < 1)
         {
             Toast.makeText(this, "Please enter a client partition ID between 1 and 10 (inclusive)", Toast.LENGTH_LONG).show();
         }
-        else{
-            hideKeyboard(this);
-            Toast.makeText(this, "Loading the local training dataset in memory.", Toast.LENGTH_LONG).show();
-//            loadDataBtn.setEnabled(false);
-
-            ExecutorService executor = Executors.newSingleThreadExecutor();
-            Handler handler = new Handler(Looper.getMainLooper());
-            String dataFile = "client_" + client.getText() + ".bin";
-
-            executor.execute(new Runnable() {
-                private String result;
-
-                @Override
-                public void run() {
-                    try {
-                        AssetFileDescriptor fileDescriptor = getApplicationContext().getAssets().openFd("data/"+dataFile);
-                        FileInputStream stream = fileDescriptor.createInputStream();
-                        boolean isSucess = dataLoader(stream);
-                        if (isSucess) {
-                            result = "Training dataset is loaded in memory.";
-                        }
-                        else {
-                            result = "Failed to load training dataset";
-                        }
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        result = "Exception occurred!";
-                    }
-                    handler.post(() -> {
-                        setResultText(result);
-                        startTrainingBtn.setEnabled(true);
-                    });
-                }
-            });
+        else if (TextUtils.isEmpty(nEpochs.getText().toString())) {
+            Toast.makeText(this, "Please enter training epochs", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            url = "http://" + address.getText().toString() + ":" + port.getText().toString();
+            String client_id = String.valueOf(client.getText());
+            setResultText("Connecting to server...");
+            sendRequest(GET, "connect", "client_id", client_id);
         }
     }
-    void sendRequest(String type, String method, String paramname, String param){
+    public void loadData(View view) {
+        hideKeyboard(this);
+        Toast.makeText(this, "Loading the local training dataset in memory.", Toast.LENGTH_LONG).show();
 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        String dataFile = "client_" + client.getText() + ".bin";
+
+        executor.execute(new Runnable() {
+            private String result;
+
+            @Override
+            public void run() {
+                try {
+                    AssetFileDescriptor fileDescriptor = getApplicationContext().getAssets().openFd("data/"+dataFile);
+                    FileInputStream stream = fileDescriptor.createInputStream();
+                    boolean isSucess = dataLoader(stream);
+                    if (isSucess) {
+                        result = "Training dataset is loaded in memory.";
+                    }
+                    else {
+                        result = "Failed to load training dataset";
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    result = "Exception occurred! Failed to load training dataset";
+                }
+                handler.post(() -> {
+                    setResultText(result);
+                    loadDataBtn.setEnabled(false);
+                    startTrainingBtn.setEnabled(true);
+                });
+            }
+        });
+    }
+    public void load_and_train(int current_round) throws IOException  {
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReference();
+
+        // Create a reference with an initial file path and name
+        String path = String.format("%s/Round_%d/global_model.bin", project_name, current_round);
+        Log.d("PATH", path);
+        StorageReference pathReference = storageRef.child(path);
+        File localFile = new File(getApplicationContext().getFilesDir(), "global_model.bin");
+        pathReference.getFile(localFile).addOnCompleteListener(new OnCompleteListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<FileDownloadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+                    setResultText("Global Model for round " + String.valueOf(current_round) + " is downloaded");
+//                    tl.loadParameters(localFile);
+                    setResultText("Training started..");
+                    tl.startTraining();
+
+                    String global_model = "global_model_" + String.valueOf(current_round) + ".bin";
+                    File globalModelFile = new File(getApplicationContext().getFilesDir(), global_model);
+
+                    // save weights (replace existing file)
+                    if (globalModelFile.exists())
+                        globalModelFile.delete();
+                    setResultText("Training done! Save and upload model to server.");
+                    tl.saveParameters(globalModelFile);
+                    uploadParameters(globalModelFile);
+                    // close TL
+                    tl.close();
+                    tl = null;
+                    uploadParameters(globalModelFile);
+                } else {
+                    if (!task.isSuccessful()) {
+                        Log.e("DOWNLOAD_FAILURE", "Error getting data", task.getException());
+                    }
+                    setResultText("Failure to download global model!");
+                }
+            }
+        });
+    }
+    public void startTraining(View view) throws IOException {
+        load_and_train(current_round);
+    }
+    void sendRequest(String type, String method, String paramname, String param){
         /* if url is of our get request, it should not have parameters according to our implementation.
          * But our post request should have 'name' parameter. */
         String fullURL=url+"/" + method + (param==null?"":"/"+param);
+        Log.d("URL", fullURL);
         Log.d(TAG, fullURL);
         Request request = null;
 
@@ -179,11 +238,24 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onResponse(Call call, final Response response) throws IOException {
-                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                if (!response.isSuccessful()) {
+                    setResultText("Response code " + response);
+                    throw new IOException("Unexpected code " + response);
+                }
                 else {
-                    if (method.equals("check")) {
-                        final String responseData = response.body().string();
+                    if (method.equals("connect")) {
+                        String responseData = response.body().string();
                         setResultText(responseData);
+                        if (responseData.startsWith("Successful")) {
+                            project_name = responseData.split(" ")[4];
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    connectBtn.setEnabled(false);
+                                    loadDataBtn.setEnabled(true);
+                                }
+                            });
+                        }
                     }
                 }
             }
@@ -239,9 +311,26 @@ public class MainActivity extends AppCompatActivity {
         dataManager.close();
         return true;
     }
-    public void startTraining(View view) {
-        tl.startTraining();
-//        tl.saveParameters();
-    }
+    public void uploadParameters(File model_weight) {
+        StorageReference storageRef = storage.getReference();
+        Uri file = Uri.fromFile(model_weight);
+        String path = String.format("%s/Round_%d/client_%d.bin", project_name, current_round, String.valueOf(client));
+        Log.d("PATH", path);
+        StorageReference ref = storageRef.child(path);
+        UploadTask uploadTask = ref.putFile(file);
 
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                exception.printStackTrace();
+                setResultText("Failure to upload weight");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                setResultText("Weight uploaded to server");
+            }
+        });
+    }
 }
